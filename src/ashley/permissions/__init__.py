a83311c4c827082d8da7b5e6ff0845e456214d51
Matthieu Huguet <matthieu.huguet@fun-mooc.fr>
2020-03-16 18:23:53 +0100

    ✨(ashley) add extra permission to course administrators
    Users having specific LTI roles (Administrator or Instructor) are
    given extra permissions to be able to moderate their forum.

--- src/ashley/permissions/__init__.py
+++ src/ashley/permissions/__init__.py
@@ -10,6 +10,18 @@ DEFAULT_GROUP_FORUM_PERMISSIONS = [
     "can_edit_own_posts",
     "can_post_without_approval",
     "can_vote_in_polls",
-    "can_attach_file",
-    "can_download_file",
 ]
+
+DEFAULT_ADMIN_GROUP_FORUM_PERMISSIONS = DEFAULT_GROUP_FORUM_PERMISSIONS + [
+    "can_post_announcements",
+    "can_post_stickies",
+    "can_delete_own_posts",
+    "can_create_polls",
+    "can_lock_topics",
+    "can_edit_posts",
+    "can_delete_posts",
+    "can_approve_posts",
+    "can_reply_to_locked_topics",
+]
+
+DEFAULT_INSTRUCTOR_GROUP_FORUM_PERMISSIONS = DEFAULT_ADMIN_GROUP_FORUM_PERMISSIONS
