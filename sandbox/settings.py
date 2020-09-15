34e9f36827d6ad44955495f2049c9756a01153b7
Sydney TAP <sydney.tap@gmail.com>
2020-09-15 16:02:59 +0200

    ⬆️(project) add elasticsearch container and dependency
    add this container since it is needed for the search feature

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -201,7 +201,11 @@ class Base(Configuration):
 
     # Search engine
     HAYSTACK_CONNECTIONS = {
-        "default": {"ENGINE": "haystack.backends.simple_backend.SimpleEngine"},
+        'default': {
+            'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+            'URL': 'http://127.0.0.1:9200/',
+            'INDEX_NAME': 'haystack',
+        },
     }
 
     # Machina
