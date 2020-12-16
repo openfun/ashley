c83aa904b283135edd902d8f9dffafd5cc0606af
Matthieu Huguet <matthieu.huguet@fun-mooc.fr>
2020-12-16 18:23:11 +0100

    ✨(profile) allow users with empty username to define it
    Some users may have empty public_username, if it has not been found in
    the LTI launch request. These users should be able to define it to
    setup their profile.

--- src/ashley/views.py
+++ src/ashley/views.py
@@ -4,18 +4,30 @@ import logging
 from typing import List
 
 from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from django.urls import reverse
 from django.utils import translation
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import UpdateView
 from lti_toolbox.lti import LTI
 from lti_toolbox.views import BaseLTIAuthView
 from machina.apps.forum_permission.shortcuts import assign_perm
+from machina.apps.forum_permission.viewmixins import (
+    PermissionRequiredMixin as BasePermissionRequiredMixin,
+)
 from machina.core.db.models import get_model
+from machina.core.loading import get_class
 
 from .defaults import DEFAULT_FORUM_BASE_PERMISSIONS, DEFAULT_FORUM_ROLES_PERMISSIONS
 
 Forum = get_model("forum", "Forum")  # pylint: disable=C0103
 LTIContext = get_model("ashley", "LTIContext")  # pylint: disable=C0103
+PermissionRequiredMixin: BasePermissionRequiredMixin = get_class(
+    "forum_permission.viewmixins", "PermissionRequiredMixin"
+)
+User = get_user_model()
 
 logger = logging.getLogger(__name__)
 
@@ -37,6 +49,7 @@ class ForumLTIView(BaseLTIAuthView):
         # Synchronize the user groups related to the current LTI context
         context.sync_user_groups(self.request.user, lti_request.roles)
 
+        # Get or create the requested forum
         forum_uuid = self.kwargs["uuid"]
         forum, forum_created = Forum.objects.get_or_create(
             lti_id=forum_uuid,
@@ -47,9 +60,14 @@ class ForumLTIView(BaseLTIAuthView):
             logger.debug("Forum %s created", forum_uuid)
             self._init_forum(forum, context)
 
-        response = HttpResponseRedirect(
-            reverse("forum:forum", kwargs={"slug": forum.slug, "pk": forum.id})
-        )
+        if not getattr(self.request.user, "public_username", ""):
+            redirect_url = reverse("forum.username.change")
+        else:
+            redirect_url = reverse(
+                "forum:forum", kwargs={"slug": forum.slug, "pk": forum.id}
+            )
+
+        response = HttpResponseRedirect(redirect_url)
 
         locale = lti_request.get_param("launch_presentation_locale")
         if locale:
@@ -87,3 +105,32 @@ class ForumLTIView(BaseLTIAuthView):
                 forum.lti_id,
             )
             assign_perm(perm, group, forum, True)
+
+
+class ChangeUsernameView(PermissionRequiredMixin, UpdateView):
+    """ Allow the current user to update his username. """
+
+    model = User
+    fields = ["public_username"]
+    template_name = "ashley/change_username.html"
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        # pylint: disable=attribute-defined-outside-init
+        self.object = form.save()
+        messages.success(
+            self.request,
+            _(
+                "Welcome %(username)s, your username is now registered."
+                % {"username": self.request.user.public_username}
+            ),
+        )
+        return HttpResponseRedirect(reverse("forum:index"))
+
+    # pylint: disable=no-self-use,unused-argument
+    def perform_permissions_check(self, user, obj, perms):
+        """ The user can update his username only if it is not yet defined """
+        return user.is_authenticated and not getattr(obj, "public_username", "")
