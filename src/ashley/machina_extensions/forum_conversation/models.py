29560d7e757f6ca4fff29517ef7b1dfa1315f528
carofun <caroline.cramoisan@fun-mooc.fr>
2021-02-26 09:59:30 +0100

    ✨(forum) sort discussion topics
    The view listing the different topics in the forum is by default ordered by
    last messages which enables to see recent activity. If we want to see which
    subjects in the past were dynamic, it’s interesting to be able to sort
    discussions from other parameters such as number of replies or views. On the
    other hand, if we have a lot of history, we still want to focus on the latest
    posts, so it’s interesting to combine sorting fields. For this purpose, this
    feature introduces sorting by multiple columns. This source code is inspired
    and done the same way it’s done in Django’s admin.
    
    Resolve #66

--- src/ashley/machina_extensions/forum_conversation/models.py
+++ src/ashley/machina_extensions/forum_conversation/models.py
@@ -1,4 +1,38 @@
 """Declare the models related to the forum_conversation app ."""
 
-# Import models from the django-machina app
-from machina.apps.forum_conversation.models import *  # noqa, pylint: disable=unused-wildcard-import,wildcard-import
+from django.db import models  # pylint: disable=all
+from django.utils.translation import gettext_lazy as _
+from machina.apps.forum_conversation.abstract_models import AbstractTopic
+
+# Custom models should be declared before importing
+# django-machina models
+
+
+class Topic(AbstractTopic):
+    # The number of posts included in this topic (only those that are approved).
+    # Add Index for sorting
+    posts_count = models.PositiveIntegerField(
+        editable=False,
+        blank=True,
+        default=0,
+        verbose_name=_("Posts count"),
+        db_index=True,
+    )
+    # Custom models should be declared before importing
+    # django-machina models
+
+    # The number of time the topic has been viewed.
+    # Add Index for sorting
+    views_count = models.PositiveIntegerField(
+        editable=False,
+        blank=True,
+        default=0,
+        verbose_name=_("Views count"),
+        db_index=True,
+    )
+
+    class Meta(AbstractTopic.Meta):
+        abstract = True
+
+
+from machina.apps.forum_conversation.models import *  # noqa
