df3bf2da48a521ea00846c268164a37779378e1f
Matthieu Huguet <matthieu.huguet@fun-mooc.fr>
2021-06-17 19:24:35 +0200

    ✨(terraform) add terraform configuration
    In a near future, we want to store ashley medias and static files on
    AWS. We want to use Terraform to setup the required AWS resources for
    each environment.

---
+++ terraform/create_state_bucket/s3.tf
@@ -0,0 +1,25 @@
+resource "aws_kms_key" "state_key" {
+  description = "Used by Terraform to store remote state for ashley"
+}
+
+resource "aws_s3_bucket" "state_bucket" {
+  bucket_prefix = "ashley-terraform"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.state_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  tags = {
+    Name = "terraform"
+  }
+}
