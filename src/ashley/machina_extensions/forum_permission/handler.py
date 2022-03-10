43fc9e7bc7c8245fcf77ff76d1209624c2672bbd
carofun <caroline.cramoisan@fun-mooc.fr>
2022-03-10 16:03:46 +0100

    🐛(back) search queries restricted on LTIContext
    If a user was part of multiple courses, search queries were
    taking a lot of processing time. DjangoMachina checks the
    permissions of the user for each forum he has access to. We only
    need to control that the user has the permission to read the forums
    part of the current LTIContext. By limiting this query, we fix
    at the same time, the problem of the processing time.
    With this new method overridden from DjangoMachina, we exclude as
    well archived forums.

--- src/ashley/machina_extensions/forum_permission/handler.py
+++ src/ashley/machina_extensions/forum_permission/handler.py
@@ -49,9 +49,10 @@ class PermissionHandler(BasePermissionHandler):
         """
         forums_to_show = super().forum_list_filter(qs, user)
         if self.current_lti_context_id:
-            return forums_to_show.filter(archived=False).filter(
-                lti_contexts__id=self.current_lti_context_id
+            return forums_to_show.filter(
+                archived=False, lti_contexts__id=self.current_lti_context_id
             )
+
         return forums_to_show
 
     def get_readable_forums(self, forums, user):
@@ -61,24 +62,104 @@ class PermissionHandler(BasePermissionHandler):
 
         We override django machina's method to filter forums based on the
         current LTI context of the User, if any.
+
         """
-        readable_forums = super().get_readable_forums(forums, user)
+
+        if user.is_superuser:
+            readable_forums = forums.filter(archived=False)
+        else:
+            # Fetches the forums that can be read by the given user.
+            readable_forums = self._get_forums_for_user_with_lti_forums(
+                forums,
+                user,
+                [
+                    "can_read_forum",
+                ],
+                True,
+            )
+            readable_forums = (
+                forums.filter(id__in=[f.id for f in readable_forums])
+                if isinstance(forums, (models.Manager, models.QuerySet))
+                else list(filter(lambda f: f in readable_forums, forums))
+            )
+
         if self.current_lti_context_id:
-            if isinstance(forums, (models.Manager, models.QuerySet)):
+            if isinstance(readable_forums, (models.Manager, models.QuerySet)):
                 return readable_forums.filter(
-                    lti_contexts__id=self.current_lti_context_id
+                    archived=False, lti_contexts__id=self.current_lti_context_id
                 )
             return list(
                 filter(
                     lambda f: any(
                         x
                         for x in f.lti_contexts.all()
-                        if x.id == self.current_lti_context_id
+                        if x.id == self.current_lti_context_id and not f.archived
                     ),
                     readable_forums,
                 )
             )
+
         return readable_forums
+
+    def _get_forums_for_user(self, user, perm_codenames, use_tree_hierarchy=False):
+        """Returns all the forums that satisfy the given list of permission codenames.
+
+        User and group forum permissions are used.
+
+        If the ``use_tree_hierarchy`` keyword argument is set the granted forums will be filtered
+        so that a forum which has an ancestor which is not in the granted forums set will not be
+        returned.
+
+        We override django machina's method to filter forums based on the
+        current LTI context of the User, if any and to automatically exclude archived forums.
+
+        """
+        forums = Forum.objects.filter(archived=False)
+        if self.current_lti_context_id:
+            forums = forums.filter(lti_contexts__id=self.current_lti_context_id)
+        return self._get_forums_for_user_with_lti_forums(
+            forums, user, perm_codenames, use_tree_hierarchy
+        )
+
+    def _get_forums_for_user_with_lti_forums(
+        self, filtered_forums, user, perm_codenames, use_tree_hierarchy=False
+    ):
+        """Returns for the list of forum based on filtered_forums that satisfy the given
+        list of permission codenames.
+
+        User and group forum permissions are used.
+
+        If the ``use_tree_hierarchy`` keyword argument is set the granted forums will be filtered
+        so that a forum which has an ancestor which is not in the granted forums set will not be
+        returned.
+
+        """
+        granted_forums_cache_key = "{}__{}".format(
+            ":".join(perm_codenames),
+            user.id if not user.is_anonymous else "anonymous",
+        )
+
+        if granted_forums_cache_key in self._granted_forums_cache:
+            return self._granted_forums_cache[granted_forums_cache_key]
+
+        # First check if the user is a superuser and if so, returns the forum queryset immediately.
+        if user.is_superuser:  # pragma: no cover
+            self._granted_forums_cache[granted_forums_cache_key] = filtered_forums
+            return filtered_forums
+
+        checker = self._get_checker(user)
+        perms = checker.get_perms_for_forumlist(filtered_forums, perm_codenames)
+        allowed_forums = []
+        # Check if the requested permissions are in the set of permissions for the forum
+        for _f in filtered_forums:
+            if set(perm_codenames).issubset(perms[_f]):
+                allowed_forums.append(_f)
+
+        if use_tree_hierarchy:
+            allowed_forums = self._filter_granted_forums_using_tree(allowed_forums)
+
+        self._granted_forums_cache[granted_forums_cache_key] = allowed_forums
+        return allowed_forums
 
     # pylint:disable = W0201
     def _get_all_forums(self):
